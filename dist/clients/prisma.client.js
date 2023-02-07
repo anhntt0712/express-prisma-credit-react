6dc2e8a813e07b246ac6ed1eaef191384537d770
anhntt <anh.nguyen@pionero.io>
2023-02-08 01:20:06 +0700

    delete dist

--- dist/clients/prisma.client.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prismaTodo = exports.prismaUser = void 0;
-const client_1 = require("@prisma/client");
-const { user, todo } = new client_1.PrismaClient();
-exports.prismaUser = user;
-exports.prismaTodo = todo;
