d66a825f356ab7314083223635f1474857e7804b
anhntt <anh.nguyen@pionero.io>
2023-02-11 12:18:00 +0700

    add redis

--- src/redis/index.ts
+++ src/redis/index.ts
@@ -1,5 +1,5 @@
 import cache from "express-redis-cache";
 export const redisCache = cache({
-  host: process.env.REDIS_HOST || "127.0.0.1",
-  port: process.env.REDIS_PORT || "6379",
+  host: process.env.REDIS_HOST ,
+  port: process.env.REDIS_PORT,
 });
