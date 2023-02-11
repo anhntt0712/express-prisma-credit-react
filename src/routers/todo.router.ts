cd8cbd601b796c7706523c0365ef4e0d1b7e729c
anhntt <anh.nguyen@pionero.io>
2023-02-11 10:52:05 +0700

    add redis

--- src/routers/todo.router.ts
+++ src/routers/todo.router.ts
@@ -1,14 +1,30 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import { verifyToken } from "../middlewares/verify-token.middleware";
 import { refreshToken } from "../controllers/refresh-token.controller";
-import { createTodo, deleteTodo, getTodoById, getTodos, updateTodo } from "../controllers/todo.controller";
- 
+import {
+  createTodo,
+  deleteTodo,
+  getTodoById,
+  getTodos,
+  updateTodo,
+} from "../controllers/todo.controller";
+import { redisCache } from "../redis";
+
 const router = express.Router();
- 
-router.get('/', verifyToken, getTodos);
-router.get('/:id', verifyToken, getTodoById);
-router.delete('/:id', verifyToken, deleteTodo);
-router.put('/:id', verifyToken, updateTodo)
-router.post('/', verifyToken, createTodo)
+
+router.get("/", verifyToken, getTodos);
+router.get(
+  "/:id",
+  verifyToken,
+  (req: Request, res: Response, next) => {
+    res.express_redis_cache_name = "todos-" + req.params.id;
+    next();
+  },
+  redisCache.route(),
+  getTodoById
+);
+router.delete("/:id", verifyToken, deleteTodo);
+router.put("/:id", verifyToken, updateTodo);
+router.post("/", verifyToken, createTodo);
 
 export default router;
